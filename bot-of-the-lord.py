f8e9f4c560c66c0c8eaa50e5d977cb311daeea40
triplequestionmark <33409863+triplequestionmark@users.noreply.github.com>
2018-02-26 20:35:12 -0800

    Update bot-of-the-lord.py

--- bot-of-the-lord.py
+++ bot-of-the-lord.py
@@ -10,6 +10,7 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
 import time
+import os
 
 Client = discord.Client()
 client = commands.Bot(command_prefix = "?")
